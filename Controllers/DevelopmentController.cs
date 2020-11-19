d31a8433499c6e4ee4de54123960134588b9cab0
castor-dev2 <fabio.oquendo@castor.com.co>
2020-11-19 17:15:59 -0500

    first commit

---
+++ Controllers/DevelopmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LibraryMVC.Models;
+
+namespace LibraryMVC.Controllers
+{
+    public class DevelopmentController : Controller
+    {
+        private readonly ILogger<DevelopmentController> _logger;
+
+        public DevelopmentController(ILogger<DevelopmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
